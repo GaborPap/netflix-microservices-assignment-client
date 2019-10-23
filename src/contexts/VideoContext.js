016f553365caeaafd9bdc4b8372e0e1ba639a1c3
GaborPap <ppgabor@gmail.com>
2019-10-24 00:59:38 +0200

    Implemented context to store videos.

---
+++ src/contexts/VideoContext.js
@@ -0,0 +1,36 @@
+import React, {useReducer, createContext, useEffect, useState} from 'react';
+import axios from 'axios';
+import videoReducer from '../reducers/VideoReducer'
+
+export const videoContext = createContext();
+
+export default function VideoContextProvider(props){
+    const initialState = [
+        {
+            id : 0,
+            name : '',
+            url: ''
+        }
+     ]
+
+
+    const [video, dispatch] = useReducer(videoReducer, initialState);
+
+    useEffect( () => {
+    fetchData();
+    }, [])
+
+    const fetchData = () => {
+    axios.get("http://localhost:8762/videos/allVideos")
+        .then(res => {
+            const data = res.data;
+            dispatch({type: "STORE_DATA", data})
+        })
+    };
+
+    return (
+        <videoContext.Provider value={{video, dispatch, fetchData}}>
+            {props.children}
+            </videoContext.Provider>
+    )
+}
