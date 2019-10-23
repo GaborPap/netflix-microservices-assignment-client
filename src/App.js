b0806960e673d871bd0ed73d9e2a29a8c9e2dc25
GaborPap <ppgabor@gmail.com>
2019-10-24 01:04:06 +0200

    Implemented routing for the page.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoList from "./video/VideoList";
+
+
+function App() {
+  return (
+    <div className="App">
+     <VideoList/>
+    </div>
+  );
+}
+
+export default App;
