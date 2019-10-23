8d108c7189f9c3cecfb69401c3a5cd901a688c76
GaborPap <ppgabor@gmail.com>
2019-10-24 01:00:59 +0200

    Implemented context to store recommendations.

---
+++ src/contexts/RecommendContext.js
@@ -0,0 +1,44 @@
+import React, {createContext, useReducer} from 'react';
+import axios from 'axios';
+import recommendReducer from "../reducers/RecommendReducer";
+
+export const recommendContext = createContext();
+
+
+function RecommendContextProvider(props) {
+    const intialState = [
+        {
+            video : {
+                id : 0,
+                name : '',
+                url: ''
+            },
+            recommendations :[
+                {
+                    id : 0,
+                    rating : 0,
+                    comment: 'a',
+                    videoId : 0
+                }
+            ]
+        }
+    ];
+
+    const [rec, dispatch] = useReducer(recommendReducer, intialState);
+
+    const fetchData = (videoId) => {
+        axios.get("http://localhost:8762/videos/video/"+videoId)
+            .then(res => {
+                const data = res.data;
+                dispatch({type: "STORE_DATA", data})
+            })
+    };
+
+    return (
+        <recommendContext.Provider value={{rec, dispatch, fetchData}}>
+            {props.children}
+        </recommendContext.Provider>
+    );
+}
+
+export default RecommendContextProvider;
